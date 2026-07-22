683cd78e8fdbd6de460989703ec53bea20f0b3fa
huihuixiao <huihuixiao@tencent.com>
2026-07-22 11:35:51 +0800

    amount covers all fees test 2

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   distDir: process.env.BUILD_OUTPUT_DIR,
   experimental: {
     optimizePackageImports: ['@chakra-ui/react'],
